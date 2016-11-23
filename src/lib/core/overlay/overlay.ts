4b6c71b4d08e4ad2786b519e8bb7ef186b42412a
curio hoiho <curiohoiho@users.noreply.github.com>
2016-11-22 16:26:10 -0800

    going through overlay.ts

---
+++ src/lib/core/overlay/overlay.ts
@@ -0,0 +1,9 @@
+import { ComponentFactoryResolver, Injectable, ApplicationRef, Injector } from '@angular/core';
+import { OverlayState } from './overlay-state';
+import { DomPortalHost } from '../portal/dom-portal-host';
+import { OverlayRef } from './overlay-ref';
+
+import { OverlayPositionBuilder } from './position/overlay-position-builder';
+import { ViewportRuler } from './position/viewport-ruler';
+import { OverlayContainer } from './overlay-container';
+
